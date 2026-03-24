4b00fa4999a0e6f7df8706208df16d41abb4f784
KevinNielsen00 <122385622+KevinNielsen00@users.noreply.github.com>
2026-03-24 16:09:05 +0100

    prep docker deploy

--- UptimeDaddy.API/UptimeDaddy.API/Program.cs
+++ UptimeDaddy.API/UptimeDaddy.API/Program.cs
@@ -39,14 +39,11 @@ using (var scope = app.Services.CreateScope())
 
 app.UseCors();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "UptimeDaddy API V1");
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "UptimeDaddy API V1");
+});
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
