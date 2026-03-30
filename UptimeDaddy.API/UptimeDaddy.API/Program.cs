40606ef99773b75ff9dca8d51a63b2bd326047d9
KevinNielsen00 <122385622+KevinNielsen00@users.noreply.github.com>
2026-03-30 15:43:10 +0200

    test

--- UptimeDaddy.API/UptimeDaddy.API/Program.cs
+++ UptimeDaddy.API/UptimeDaddy.API/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
                 "http://localhost:5173",
-                "http://10.133.51.121:5173"
+                "10.133.51.121:5173"
             )
             .AllowAnyHeader()
             .AllowAnyMethod();
