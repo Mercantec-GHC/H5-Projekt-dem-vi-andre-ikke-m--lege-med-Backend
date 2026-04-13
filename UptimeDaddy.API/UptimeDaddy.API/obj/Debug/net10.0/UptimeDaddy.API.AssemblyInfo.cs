1404230a947908d9ea7068cfe721c63f6275c291
KevinNielsen00 <122385622+KevinNielsen00@users.noreply.github.com>
2026-04-13 14:15:01 +0200

    removed README in the wrong location

--- UptimeDaddy.API/UptimeDaddy.API/obj/Debug/net10.0/UptimeDaddy.API.AssemblyInfo.cs
+++ UptimeDaddy.API/UptimeDaddy.API/obj/Debug/net10.0/UptimeDaddy.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UptimeDaddy.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7bb7fd7df9bc4c67e0582c605f6edd22cdcd0d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec5a002c4c2290a1db188de219ee7361b3b6a064")]
 [assembly: System.Reflection.AssemblyProductAttribute("UptimeDaddy.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UptimeDaddy.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- UptimeDaddy.API/UptimeDaddy.API/obj/Release/net10.0/UptimeDaddy.API.AssemblyInfo.cs
+++ UptimeDaddy.API/UptimeDaddy.API/obj/Release/net10.0/UptimeDaddy.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UptimeDaddy.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc75aab14780e57eddab00263f25774b71f48d38")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec5a002c4c2290a1db188de219ee7361b3b6a064")]
 [assembly: System.Reflection.AssemblyProductAttribute("UptimeDaddy.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UptimeDaddy.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
